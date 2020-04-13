429250cbf91032b6c2d697a910b40f0b4d56fa70
guoyanhong <1352118502@qq.com>
2020-04-13 23:48:54 +0800

    chore

--- test/unit/webpack-base-test.js
+++ test/unit/webpack-base-test.js
@@ -8,7 +8,7 @@ describe('webpack.base.js test case', () => {
 
   // eslint-disable-next-line no-undef
   it('entry', () => {
-    assert.equal(baseConfig.entry.index, '/Users/coco/project/webpack/build-webpack/test/smoke/template/src/index/index.js');
-    assert.equal(baseConfig.entry.search, '/Users/coco/project/webpack/build-webpack/test/smoke/template/src/search/index.js');
+    assert.equal(baseConfig.entry.index.indexOf('build-webpack/test/smoke/template/src/index/index.js') !== -1, true);
+    assert.equal(baseConfig.entry.search.indexOf('build-webpack/test/smoke/template/src/search/index.js') !== -1, true);
   });
 });
