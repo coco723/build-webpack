a7f8b37f96f5eb7ec8ed3a4c98ec7f86c976c6e5
guoyanhong <1352118502@qq.com>
2020-04-13 22:52:50 +0800

    chore

--- test/unit/webpack-base-test.js
+++ test/unit/webpack-base-test.js
@@ -8,7 +8,7 @@ describe('webpack.base.js test case', () => {
 
   // eslint-disable-next-line no-undef
   it('entry', () => {
-    assert.equal(baseConfig.entry.index, '/Users/coco/project/log/2020/webpack/code/chapter04/my-project/build-webpack/test/smoke/template/src/index/index.js');
-    assert.equal(baseConfig.entry.search, '/Users/coco/project/log/2020/webpack/code/chapter04/my-project/build-webpack/test/smoke/template/src/search/index.js');
+    assert.equal(baseConfig.entry.index, '/Users/coco/project/webpack/build-webpack/test/smoke/template/src/index/index.js');
+    assert.equal(baseConfig.entry.search, '/Users/coco/project/webpack/build-webpack/test/smoke/template/src/search/index.js');
   });
 });
